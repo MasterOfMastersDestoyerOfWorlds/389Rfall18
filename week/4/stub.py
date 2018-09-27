a47728da683b0c479b8d91d31ddc4f1ad0ebd68f
Andrew Wollack <awollac@wireless-10-105-231-237.umd.edu>
2018-09-27 18:50:46 -0400

    finished hw4

--- week/4/stub.py
+++ week/4/stub.py
@@ -12,26 +12,85 @@ import socket
 host = "cornerstoneairlines.co" # IP address here
 port = 45 # Port here
 
-def execute_cmd(cmd):
-    """
-        Sockets: https://docs.python.org/2/library/socket.html
-        How to use the socket s:
+def usage():
+	print("shell Drop into an interactive shell and allow users to gracefully exit")
+	print("pull  <remote-path> <local-path> Download files")
+	print("help  Shows this help menu")
+	print("quit  Quit the shell")
 
-            # Establish socket connection
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((host, port))
-
-            Reading:
-
-                data = s.recv(1024)     # Receives 1024 bytes from IP/Port
-                print(data)             # Prints data
-
-            Sending:
-
-                s.send("something to send\n")   # Send a newline \n at the end of your command
-    """
-    print("IMPLEMENT ME")
-
+def execute_cmd(cmd_args, directory = None):
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((host, port))
+	if(directory is not None):
+		s.send(";" + "cd " + directory + ";"+ cmd_args + "\n")
+	else:
+		s.send(";" + cmd_args + "\n")
+	s.recv(1024)
+	data = s.recv(1024)
+	
+	s.close()
+	return data
 
 if __name__ == '__main__':
-    print("IMPLEMENT ME")
+	directory = "/"
+	exit = True
+	shell = False
+	while exit:
+		print(">"),
+
+		input = raw_input()
+		pull_request = input.split(' ')
+
+		if(input == "shell"):
+			shell = True
+		elif(input == "quit"):
+			exit = False
+		elif(input == "help"):
+			usage()
+		elif(pull_request[0] == "pull" and len(pull_request) == 3):
+			#check if file exists on remote and write to file on local
+			if(execute_cmd("if [ -f " + pull_request[1] + " ]; then echo true; fi", "/") == "true\n"):
+				fh = open(pull_request[2], 'w')
+				fh.write(execute_cmd("cat " + pull_request[1]))
+				fh.close
+			else:
+				print("remote-path not found")
+		else:
+			usage()
+
+		while shell:
+
+			print(directory + ">"),
+
+			input = raw_input()
+			cmd_args = input.split(' ')
+
+			if(cmd_args[0] == "cd" and len(cmd_args) == 2):
+
+				dir_parts = cmd_args[1].split('/')
+				#check for ./ or ../
+				if(cmd_args[1][0:1] != '/'):
+					temp_dir = directory.split('/')
+
+				for x in range(len(dir_parts)):
+					if(dir_parts[x] == ".."):
+						temp_dir.pop()
+					else:
+						temp_dir.append(dir_parts[x])
+
+				temp_dir_str = ""
+				for x in range(len(temp_dir)):
+					if(temp_dir[x] != ''):
+						temp_dir_str = temp_dir_str + "/" + temp_dir[x]
+				#check if directory exists
+				if(execute_cmd("if [ -d " + temp_dir_str + " ]; then echo true; fi", "/") == "true\n"):
+					if(temp_dir_str != ""):
+						directory = temp_dir_str
+					else:
+						directory = "/"
+				else:
+					print("Directory does not exist")
+			elif(cmd_args[0] == "quit"):
+				shell = False
+			else:
+				print(execute_cmd(input, directory))
