82a1f568fb3f4b88d85421b556397e7a23746fdd
Andrew Wollack <awollac@wireless-10-105-225-205.umd.edu>
2018-11-01 00:18:30 -0400

    finished hw 7

--- week/8/stub.py
+++ week/8/stub.py
@@ -2,6 +2,8 @@
 
 import sys
 import struct
+import datetime
+import os
 
 
 # You can use this method to exit on failure conditions.
@@ -24,7 +26,11 @@ with open(sys.argv[1], 'rb') as fpff:
 # Hint: struct.unpack will be VERY useful.
 # Hint: you might find it easier to use an index/offset variable than
 # hardcoding ranges like 0:8
-magic, version = struct.unpack("<LL", data[0:8])
+magic, version, time = struct.unpack("<LLL", data[0:12])
+
+(author) = data[12:20]
+
+(count) = struct.unpack("<L", data[20:24])
 
 if magic != MAGIC:
     bork("Bad magic! Got %s, expected %s" % (hex(magic), hex(MAGIC)))
@@ -32,11 +38,85 @@ if magic != MAGIC:
 if version != VERSION:
     bork("Bad version! Got %d, expected %d" % (int(version), int(VERSION)))
 
+if count <= 0:
+	bork("Bad section count <= 0")
+
 print("------- HEADER -------")
-print("MAGIC: %s" % hex(magic))
-print("VERSION: %d" % int(version))
+print("MAGIC: %s\n" % hex(magic))
+print("VERSION: %d\n" % int(version))
+print("TIME: %s\n" % datetime.datetime.fromtimestamp(time).strftime('%Y-%m-%d %H:%M:%S'))
+print("AUTHOR: %s\n" % author)
+print("SECTION COUNT: %d\n" % count)
+print("-------  BODY  -------\n\n")
+
+length_of_sections = len(data) - 24
+offset = 24 
+section_count = 0
+while length_of_sections > 0:
+	stype, slen = struct.unpack("<LL", data[offset:offset+8])
+	offset = offset + 8
+	section_count += 1
+	if(stype == 0x1):
+		f = open(os.getcwd()+"/" + sys.argv[1] + "png_" + repr(section_count) + ".png", 'wb')
+		f.write("\211PNG\r\n\032\n")
+		f.write(data[offset:offset+slen])
+		f.close()
+		print("PNG SECTION:\n Wrote to: " + os.getcwd()+"/" + sys.argv[1] + "png_" + repr(section_count) + ".png"+"\n")
+	elif(stype == 0x2):
+		if(slen%8 == 0):
+			dwords = []
+			for x in range(0,int(slen/8)):
+				dword = struct.unpack("<Q", data[offset + (x-1) * 8:offset + x * 8])
+				dwords.append(dword)
+			print("DWORDS SECTION: \n" + repr(dwords) + "\n")
+		else:
+			bork("too many bytes")
+	elif(stype == 0x3):
+		(text) = data[offset:offset+slen]
+		print("UTF-8 SECTION: \n%s\n" % text.decode('utf-8'))
+	elif(stype == 0x4):
+		if(slen%8 == 0):
+			doubles = []
+			for x in range(0,int(slen/8)):
+				double = struct.unpack("<d", data[offset + (x-1) * 8:offset + x * 8])
+				doubles.append(double)
+			print("DOUBLES SECTION: \n" + repr(doubles) + "\n")
+		else:
+			bork("too many bytes")
+	elif(stype == 0x5):
+		if(slen%4 == 0):
+			words = []
+			for x in range(0,int(slen/4)):
+				word = struct.unpack("<L", data[offset + (x-1) * 4:offset + x * 4])
+				words.append(word)
+			print("WORDS SECTION: \n" + repr(words) + "\n")
+		else:
+			bork("too many bytes")
+	elif(stype == 0x6):
+		if(slen == 16):
+			(lat, lon) = struct.unpack("<dd", data[offset:offset+slen])
+			print("COORDINATES SECTION:\nLatitude: %f , Longitude: %f\n" % (lat,lon))
+		else:
+			bork("Bad section length != 16")
+	elif(stype == 0x7):
+		if(slen == 4):
+			(reference) = struct.unpack("<L", data[offset:offset+slen])
+			if(reference < count and reference >= 0):
+				print("SECTION REFERENCE: \n%d\n" % reference)
+			else:
+				bork("Bad section num. not in range")
+		else:
+			bork("Bad section length != 4")
+	elif(stype == 0x9):
+		(text) = data[offset:offset+slen]
+		print("ASCII SECTION: \n%s\n" % text)
+
+	offset = offset + slen
+	length_of_sections -= offset
+
+print("ACTUAL SECTION COUNT:%d" % section_count)
 
 # We've parsed the magic and version out for you, but you're responsible for
 # the rest of the header and the actual FPFF body. Good luck!
 
-print("-------  BODY  -------")
+
--- week/8/writeup/README.md
+++ week/8/writeup/README.md
@@ -13,27 +13,79 @@ Digital acknowledgement of honor pledge: *PUT YOUR NAME HERE*
 ### Part 1 (45 Pts)
 1.
 
-2.
+Yes, conerstoneairlines.co
 
-3.
+2. 
+
+laz0rh4x and c0uchpot4doz
+
+3.  
+
+c0uchpot4doz : 206.189.113.189 from London
+
+laz0rh4x : 104.248.224.85	3.429234	from north bergen new jersey
 
 4.
 
+2749
+
 5.
+
+1500 on October 25th
 
 6.
 
+https://drive.google.com/file/d/1McOX5WjeVHNLyTBNXqbOde7l8SAQ3DoI/view?usp=sharing
+
 7.
+
+October 25th at 1500
+
+To get all of this information I started to follow tcp streams until I found one that was human readable and in order to find out the traceroute command I looked at the ICMP packets
 
 ### Part 2 (55 Pts)
 
 *Report your answers to the questions about parsing update.fpff below.*
 1.
 
+2018-10-24 20:40:07
+
 2.
+
+laz0rh4x
 
 3.
 
+Says that it has 9 sections only actually has 8
+
 4.
 
+ASCII SECTION: 
+Call this number to get your flag: (422) 537 - 7946
+
+WORDS SECTION: 
+[(60,), (3,), (1,), (4,), (1,), (5,), (9,), (2,), (6,), (5,), (3,), (5,), (8,), (9,), (7,)]
+
+COORDINATES SECTION:
+Latitude: 38.991610 , Longitude: -77.027540
+
+SECTION REFERENCE: 
+1
+
+ASCII SECTION: 
+The imfamous security pr0s at CMSC389R will never find this!
+
+ASCII SECTION: 
+The first recorded uses of steganography Can be traced back to 440 BC when Herodotus Mentions two exampleS in his Histories.[3] Histicaeus s3nt a message to his vassal, Arist8goras, by sha9ving the hRead of his most trusted servan-t, "marking" the message onto his scal{p, then sending him on his way once his hair had rePrown, withl the inastructIon, "WheN thou art come to Miletus, bid _Aristagoras shave thy head, and look thereon." Additionally, demaratus sent a warning about a forthcoming attack to Greece by wrIting it dirfectly on the wooden backing oF a wax tablet before applying i_ts beeswax surFace. Wax tablets were in common use then as reusabLe writing surfAces, sometimes used for shorthand. In his work Polygraphiae Johannes Trithemius developed his so-called "Ave-Maria-Cipher" that can hide information in a Latin praise of God. "Auctor Sapientissimus Conseruans Angelica Deferat Nobis Charitas Gotentissimi Creatoris" for example contains the concealed word VICIPEDIA.[4}
+
+COORDINATES SECTION:
+Latitude: 38.991094 , Longitude: -76.932802
+
+PNG SECTION:
+ Wrote to: /Users/awollac/Documents/CMSC389R/389Rfall18/week/8/update.fpffpng_8.png
+ 
+ picture of an airplane with the flag CMSC389R-{c0rn3rst0ne_airlin3s_to_the_m00n}
+
 5.
+
+CMSC389R-{c0rn3rst0ne_airlin3s_to_the_m00n}
