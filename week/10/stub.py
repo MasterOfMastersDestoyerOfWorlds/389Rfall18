311dd1ea22c0a1d0e2c5354905167b6a8ab4a53d
Andrew Wollack <awollac@Andrews-MBP.home>
2018-11-20 22:41:19 -0500

    finished week 10

--- week/10/stub.py
+++ week/10/stub.py
@@ -1,26 +1,51 @@
 #!/usr/bin/env python2
 # from the git repo
 import md5py
+import socket
+import re
 
 #####################################
 ### STEP 1: Calculate forged hash ###
 #####################################
 
-message = ''    # original message here
-legit = ''      # a legit hash of secret + message goes here, obtained from signing a message
+message = 'Hello'    # original message here
+
+host = "142.93.118.186"   # IP address or URL
+port =  1234    # port
+
+# use these to connect to the service
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+
+# receive some data
+
+s.recv(1024)
+
+s.send('1\n')
+
+s.recv(1024)
+
+s.send(message + '\n')
+
+data = s.recv(1024)
+
+regex = re.search("Your hash: ([a-z0-9]*)", data)
+
+
+legit = regex.group(1)     # a legit hash of secret + message goes here, obtained from signing a message
+
 
 # initialize hash object with state of a vulnerable hash
 fake_md5 = md5py.new('A' * 64)
 fake_md5.A, fake_md5.B, fake_md5.C, fake_md5.D = md5py._bytelist2long(legit.decode('hex'))
 
-malicious = ''  # put your malicious message here
+malicious = 'malicious'  # put your malicious message here
 
 # update legit hash with malicious message
 fake_md5.update(malicious)
 
 # fake_hash is the hash for md5(secret + message + padding + malicious)
 fake_hash = fake_md5.hexdigest()
-print(fake_hash)
 
 
 #############################
@@ -35,13 +60,23 @@ print(fake_hash)
 # (i.e. 20 char msg = 160 bits = 0xa0 = '\xa0\x00\x00\x00\x00\x00\x00\x00\x00')
 # craft padding to align the block as MD5 would do it
 # (i.e. len(secret + message + padding) = 64 bytes = 512 bits
-padding = ''
+for x in range(6, 16):
+	s.send('2\n')
+	s.recv(1024)
+	s.send(fake_hash+'\n')
+	print(s.recv(1024))
+	message_length = len(message) + x
+	padding =  "\x80" + "\x00"*((448-(8*message_length) - 1)/8)  + chr(message_length*8) + "\x00"*7
+	# payload is the message that corresponds to the hash in `fake_hash`
+	# server will calculate md5(secret + payload)
+	#                     = md5(secret + message + padding + malicious)
+	#                     = fake_hash
+	payload = message + padding + malicious
+	s.send(payload+'\n')
+	s.recv(1024)
 
-# payload is the message that corresponds to the hash in `fake_hash`
-# server will calculate md5(secret + payload)
-#                     = md5(secret + message + padding + malicious)
-#                     = fake_hash
-payload = message + padding + malicious
-
+print(legit)
+print(repr(payload))
+print(fake_hash)
 # send `fake_hash` and `payload` to server (manually or with sockets)
 # REMEMBER: every time you sign new data, you will regenerate a new secret!
--- week/10/writeup/README.md
+++ week/10/writeup/README.md
@@ -1,18 +1,33 @@
 Writeup 10 - Crypto II
 =====
 
-Name: *PUT YOUR NAME HERE*
-Section: *PUT YOUR SECTION HERE*
+Name: Andrew Wollack
+Section: 0201
 
 I pledge on my honor that I have not given or received anyunauthorized assistance on this assignment or examination.
 
-Digital acknowledgement of honor pledge: *PUT YOUR NAME HERE*
+Digital acknowledgement of honor pledge: *Andrew Wollack
 
 ## Assignment 10 Writeup
 
 ### Part 1 (70 Pts)
 
+CMSC389R-{i_still_put_the_M_between_the_DV}
+
+legit hash: 73a08cbb8734f643aeeb5cfba5905410
+
+payload: Hello\x80\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xa0\x00\x00\x00\x00\x00\x00\x00malicious
+
+fake_hash: b9d8192ba134c09017fbe631a06fc9c7
+
+To solve this problem I essentially followed the powerpoint directly and varied the length of the message byte from 6-15. I had trouble figuring out how the message sent was supposed to be formatted until I realized that on the server side would be putting the secret before the message cahnging the length of the message.
 
 ### Part 2 (30 Pts)
+
+gpg --decrypt ./message.private
+
+gpg --gen-key
+
+gpg -e -u "Andrew Wollack <andrew.wollack@gmail.com" -r "UMD Cybersecurity Club <president@csec.umiacs.umd.edu>" message.private
 
 
